48415f8ef57576945be706a32097df7d6c0b64cf
Roberto Rodríguez <krowno.iso@gmail.com>
2022-04-25 14:00:28 +0200

    añadidos los ficheros iniciales

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request,abort
+from funciones import LeerLibreria
+import os
+
+libreria = LeerLibreria()
+
+app = Flask(__name__)	
+
+@app.route('/')
+def inicio():
+    return render_template("inicio.html")
+
+@app.route('/libro/<isbn>')
+def libro_dinamico(isbn):
+    for libro in libreria:
+        if libro.get("isbn") == isbn:
+            publicado = libro.get("status")
+            nombre = libro.get("title")
+            imagen = libro.get("thumbnailUrl")
+            paginas = libro.get("pageCount")
+            autores = libro.get("authors")
+            descripcion = libro.get("shortDescription")
+            categorias = libro.get("categories")
+            return render_template("libro.html",nombre=nombre,imagen=imagen,paginas=paginas,autores=autores,descripcion=descripcion,categorias=categorias,publicado=publicado,isbn=isbn)
+    return abort(404)
+
+
+app.run("0.0.0.0",5000,debug=True)
+#port=os.environ["PORT"]
+#app.run("0.0.0.0",int(port),debug=True)
